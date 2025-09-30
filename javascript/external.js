4bde689416a3f3e2f0d97e78a5054d3957d6c9f7
Stephany Perez <steph2016perez@gmail.com>
2025-09-29 20:48:35 -0500

    Automated table creation for webmap reviews

--- javascript/external.js
+++ javascript/external.js
@@ -23,7 +23,6 @@ for (var i=0; i < arr.length; i++)
 */
 /*
 user_name = window.prompt("Enter your Name");
-
 if(user_name) {
   document.getElementById("namechange").innerText = "Hello, " + "Welcome " + user_name + "!";
 }
@@ -38,3 +37,19 @@ function welcome()
   return message
 }
 document.write(welcome());
+
+function webmap_table()
+{
+  document.write("<table width=100%>");
+  for (var row=0; row < 2; row++)
+  {
+    document.write("<tr>");
+    for (var column=0; column <3; column++)
+    {
+      document.write("<td>" + row + "," + column + "</td>");
+    }
+    document.write("</tr>");
+  }
+  document.write("</table>");
+  return "";
+}
